4e70aee32f52b3211153b96bbe937dba594f61f3
aayusht200 <aayush.t200@gmail.com>
2025-10-29 13:10:58 +0530

    class based library refactoring done

--- script.js
+++ script.js
@@ -6,25 +6,37 @@ class book {
     this.status = status;
     this.id = crypto.randomUUID();
   }
-  static toggleStatus() {
+  toggleStatus() {
     this.status = this.status === "READ" ? "NOT READ" : "READ";
   }
 }
 
 class library {
   constructor() {
-    const book_list = [];
+    this.book_list = [];
   }
   addBook(title, author, page, status) {
     const newBook = new book(title, author, page, status);
-    book_list.push(newBook);
+    this.book_list.push(newBook);
+  }
+  removeBook(id) {
+    this.book_list = this.book_list.filter((e) => e.id !== id);
+  }
+  toggleBookStatus(id) {
+    this.findBook(id).toggleStatus();
+  }
+  findBook(id) {
+    return this.book_list.find((e) => e.id === id);
   }
   getBookList() {
-    return book_list;
+    return this.book_list;
   }
 }
 
 let currentLibrary = new library();
-currentLibrary.addBook("one piece", "Echira Oda", 25, "READ");
-currentLibrary.addBook("one piece", "Echira Oda", 28, "NOT READ");
-console.log(currentLibrary.getBookList());
+currentLibrary.addBook("One Piece", "Eiichiro Oda", 25, "Not Read");
+currentLibrary.addBook("Naruto", "Masashi Kishimoto", 28, "Not Read");
+
+console.log(JSON.parse(JSON.stringify(currentLibrary.getBookList())));
+currentLibrary.toggleBookStatus(currentLibrary.getBookList()[0].id);
+console.log(JSON.parse(JSON.stringify(currentLibrary.getBookList())));
